6306dc1d952a85e9f76abdca76384b1ed401a839
aeniassyifa <aeniassyifamj@gmail.com>
2025-03-12 14:29:44 +0700

    update ui adminlte

--- resources/views/jnsKendaraan/tampilJnsKendaraan.blade.php
+++ resources/views/jnsKendaraan/tampilJnsKendaraan.blade.php
@@ -1,23 +1,90 @@
-<a href="{{route('jnsKendaraan.create')}}">
-    <input type="button" value="tambah">
-</a>
+@include('layout.header')
+    <!--begin::App Wrapper-->
+    <div class="app-wrapper">
+      <!--begin::Header-->
+      @include('layout.navbar')
+      
+      <!--end::Header-->
+      <!--begin::Sidebar-->
+      @include('layout.sidebar')
+      
+      <!--end::Sidebar-->
+      <!--begin::App Main-->
+      <main class="app-main">
 
-<table border = '1'>
-    <tr>
-        <td>No</td>
-        <td>NAma Jenis Kendaraan</td>
-        <td>Aksi</td>
-    </tr>
-    @php $no =1; @endphp
-    @foreach($data as $row)
-    <tr>
-        <td>{{$no++}}</td>
-        <td>{{$row->nm_jns_kendaraan}}</td>
-        <td>
-            <a href="{{route('jnsKendaraan.edit',$row->id_jns_kendaraan)}}">update</a>
-            | <a href="{{route('jnsKendaraan.delete',$row->id_jns_kendaraan)}}">delete</a>
-        </td>
-    </tr>
-    @endforeach
-    
-</table>
+      <div class="app-content-header">
+          <!--begin::Container-->
+          <div class="container-fluid">
+            <!--begin::Row-->
+            <div class="row">
+              <div class="col-sm-6"><h3 class="mb-0">Jenis kendaraan</h3></div>
+              <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-end">
+                  <li class="breadcrumb-item"><a href="#">Home</a></li>
+                  <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+                </ol>
+              </div>
+            </div>
+            <!--end::Row-->
+          </div>
+          <!--end::Container-->
+        </div>
+
+
+        <div class="app-content">
+          <!--begin::Container-->
+          <div class="container-fluid">
+            <!--begin::Row-->
+            <div class="row">
+              <div class="col-md-6">
+                <div class="card mb-4">
+                    <div class="card-header">
+                         <h3 class="card-title">Data Keseluruhan Jenis Kendaraan</h3>
+                    </div>
+                    <div class="card-body p-0">
+                                <a href="{{route('jnsKendaraan.create')}}">
+                            <input type="button" value="Tambah">
+                        </a>
+
+                        <table class="table table-striped">
+                            <tr>
+                                    <td>No</td>
+                                    <td>Nama Jenis Kendaraan</td>
+                                    <td>Aksi</td>
+                            </tr>
+                            @php $no = 1; @endphp
+                            @foreach($data as $row)
+
+                            <tr>
+                                <td>{{$no++}}</td>
+                                <td>{{$row->nm_jns_kendaraan}}</td>
+                                <td>
+                                    <a href="{{route('jnsKendaraan.edit',$row->id_jns_kendaraan)}}">update</a>  
+                                    |  
+                                    <a href="{{route('jnsKendaraan.delete',$row->id_jns_kendaraan)}}">delete</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="app-content">
+          <!--begin::Container-->
+          <div class="container-fluid">
+
+          </div>
+</section>
+</main>
+      <!--end::App Main-->
+      <!--begin::Footer-->
+      @include('layout.coba')
+      
+
+      <!--end::Footer-->
+    </div>
+    @include('layout.footer')
